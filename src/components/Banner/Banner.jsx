ccc3baf750824aebb83daabb5237f1b2ccd0a6a4
Mwaiwathu Laurent Chatha <151863592+MwaiwathuChatha@users.noreply.github.com>
2024-04-08 13:19:35 +0100

    Modified banner page

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,9 +2,21 @@ import "./Banner.scss"
 const Banner = () => {
   return (
     <div className="banner">
-        <div className="imageContainer">
-            <img src="/Mwai.png" alt="" />
+      <div className="wrapper">
+      <div className="textContainer">
+        <h2>Mwaiwathu Chatha</h2>
+        <h1> Web developer</h1>
+        <div className="buttons">
+          <button>See projects</button>
+          <button>Lets connect!</button>
+          
         </div>
+        <img src="/scroll.png" alt="" />
+      </div>
+      <div className="imageContainer">
+        <img src="/Mwai2.png" alt="" />
+      </div>
+    </div>
     </div>
   )
 }
