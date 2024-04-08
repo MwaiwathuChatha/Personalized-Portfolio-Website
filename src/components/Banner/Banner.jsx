15be569a087030675487869cbf5e81f87bffdc47
Mwaiwathu Laurent Chatha <151863592+MwaiwathuChatha@users.noreply.github.com>
2024-04-08 15:12:53 +0100

    Added animation to elements in class textContainer. Added sliding motion animation to text in class slidingText.

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,22 +1,61 @@
 import "./Banner.scss"
+import { motion }  from "framer-motion"
+
+const textVariants={
+  initial:{
+    x:-500,
+    opacity:0,
+  },
+  animate:{
+    x:0,
+    opacity:1,
+    duration:1,
+    staggerChildren:0.1,
+  },
+  scrollButton:{
+      opacity:0,
+      y:10,
+      transition:{
+       duration:1.3,
+       repeat:Infinity,
+      },
+  
+  },
+};
+const sliderVariant={
+  initial:{
+    x:0,
+  },
+  animate:{
+    x:"-800%",
+    transition:{
+      repeat:Infinity,
+      duration:40,
+    },
+  },
+}; 
 const Banner = () => {
   return (
     <div className="banner">
       <div className="wrapper">
-      <div className="textContainer">
-        <h2>Mwaiwathu Chatha</h2>
-        <h1> Web developer</h1>
-        <div className="buttons">
-          <button>See projects</button>
-          <button>Lets connect!</button>
+        <motion.div className="textContainer" variants={textVariants} initial="initial" animate="animate">
+          <motion.h2 variants={textVariants}>Mwaiwathu Chatha</motion.h2>
+          <motion.h1 variants={textVariants}> Web developer</motion.h1>
+          <motion.div variants={textVariants} className="buttons">
+            <motion.button variants={textVariants}>See projects</motion.button>
+            <motion.button variants={textVariants}>Lets connect!</motion.button>
+          </motion.div>
+          <motion.img animate="scrollButton" variants={textVariants} src="/scroll.png" alt="" />
           
+        </motion.div>
+        <motion.div variants={sliderVariant} initial="initial" animate="animate" className="slidingText">
+        Web Development Electronics UI/UX Design
+      </motion.div>
+        
+        <div className="imageContainer">
+          <motion.img  src="/Mwai2.png" alt="" />
         </div>
-        <img src="/scroll.png" alt="" />
       </div>
-      <div className="imageContainer">
-        <img src="/Mwai2.png" alt="" />
-      </div>
-    </div>
     </div>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
                 animate={{opacity:1,scale:1}} 
                 transition={{duration:0.5}}
                 >
-                    Mwaiwathus Website</motion.span>
+                    Mwai&apos;s Website</motion.span>
                 <div className="social">
                     <a href="#"><img src="/instagram.png" alt=""/></a>
                     <a href="#"><img src="/facebook.png" alt=""/></a>
