1a01c72f2a56fb3385f43fe6691138180e5bc470
Mwaiwathu Laurent Chatha <151863592+MwaiwathuChatha@users.noreply.github.com>
2024-04-09 15:20:39 +0100

    updated tech stack page

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -3,9 +3,9 @@ import "./services.scss"
 const Services = () => {
   return (
     <div className="services">
-        <div className="imageContainer">
-          <img src="/mern.png" alt="" />
-        </div>
+        
+          
+        
     </div>
   )
 }
--- src/components/techStack/TechStack.jsx
+++ src/components/techStack/TechStack.jsx
@@ -2,7 +2,34 @@ import "./techstack.scss"
 
 const TechStack = () => {
   return (
-    <div>TechStack</div>
+    <>
+      <div className="techstack">
+
+        <div className="listContainer">
+
+          <div className="box">
+            <h2>MERN Stack</h2>
+            <img src="/mern.png" alt="" />
+          </div>
+        
+
+        <div className="box">
+          <h2>Vanilla Javascript</h2>
+          <img src="/javascriptHtmlCss.png" alt="" />
+        </div>
+
+
+        <div className="box">
+          <h2>React Native</h2>
+          <img src="/reactnative.png" alt="" />
+        </div>
+        </div>
+      </div>
+
+    </>
+
+
+
   )
 }
 
